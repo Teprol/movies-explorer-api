97d1945c7add8730c36ead2ec407dcac27eb9232
Sergey <teprol112@bk.ru>
2023-09-12 00:33:14 +0400

    создал модели и схемы для api

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+// ? const NoAuthError = require('../errors/NoAuthError');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Введена некоректная почта пользователя',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
