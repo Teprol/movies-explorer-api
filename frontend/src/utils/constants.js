80e2ef84bad1f13a5306d53e63b3a56dd315b824
Sergey <teprol112@bk.ru>
2023-10-16 02:50:01 +0400

    fix/исправление замечаний ревью

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -9,6 +9,8 @@ export const initialFilmsMedium = 8;
 export const initialFilmsMini = 5;
 
 export const DesktopScreen = 1008;
+export const DesktopScreenPorogBot = 1023;
+export const DesktopScreenPorogTop = 1100;
 export const TabletScreen = 644;
 
 
